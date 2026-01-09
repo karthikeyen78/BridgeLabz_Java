8da48d8563b8a6500964df5df4a4a959851f89b1
karthikeyen78 <kkarthikeyen18@gmail.com>
2026-01-09 16:39:39 +0530

    Karthikeyen:AgeOfHarry

--- src/CoreProgramming/ProgrammingElements/level1/AgeOfHarry.java
+++ src/CoreProgramming/ProgrammingElements/level1/AgeOfHarry.java
@@ -1,4 +1,12 @@
 package CoreProgramming.ProgrammingElements.level1;
 
 public class AgeOfHarry {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
 }
