a5ef44cd343db0f1897fbd85ad6029c3380fb7a4
karthikeyen78 <kkarthikeyen18@gmail.com>
2026-01-09 16:45:58 +0530

    Karthikeyen:AverageMark

---
+++ src/CoreProgramming/ProgrammingElements/level1/SamAvgMark.java
@@ -0,0 +1,12 @@
+package CoreProgramming.ProgrammingElements.level1;
+
+public class SamAvgMark {
+    public static void main(String[] args) {
+        int math = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        double average = (math + physics + chemistry) / 3.0;
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
